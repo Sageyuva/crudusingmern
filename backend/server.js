6c87c81f085eaa979ed518e279a53221e0f9dbe6
Sageyuva <yuvarajdhammure@gmail.com>
2023-12-29 20:30:14 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,29 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-const TodoModel = require('./models/todoModel');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://yadavyuva9632:1234@cluster0.kqohcbh.mongodb.net/test");
-
-app.post('/addpost', (req, res) => {
-    TodoModel.create(req.body)
-        .then(Todos => res.json(Todos))
-        .catch(errr => res.json(err))
-})
-
-app.get('/getitems', (req, res) => {
-    TodoModel.find({})
-        .then(Todos => res.json(Todos))
-        .catch(err => res.json(err))
-})
-
-
-// app.delete('/deletetodo/:id', (res, req) => {
-//     const id = req.params.id;
-//     TodoModel.findByIdAndDelete({ _id: id })
-//         .then(Todos => res.json(Todos))
-//         .catch(err => res.json(err))
-// })
-
-app.delete('/deletetodo/:id', (req, res) => {
-    const id = req.params.id;
-    TodoModel.findByIdAndDelete({ _id: id })
-        .then(Todos => res.json(Todos))
-        .catch(err => res.json(err))
+mongoose.connect("mongodb+srv://yadavyuva9632:1234@cluster0.kqohcbh.mongodb.net/visual", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-app.get('/getpost/:id', (req, res) => {
-    const id = req.params.id;
-    TodoModel.findById({ _id: id })
-        .then(Todos => res.json(Todos))
-        .catch(err => res.json(err))
+const Visual = mongoose.connection.collection('visual');
 
-})
-
-app.put('/updatepost/:id', (req, res) => {
-    const id = req.params.id;
-    TodoModel.findByIdAndUpdate({ _id: id }, { caption: req.body.caption, heading: req.body.heading })
-        .then(Todos => res.json(Todos))
-        .catch(err => res.json(err))
-
-})
+app.get('/getitems', async(req, res) => {
+    try {
+        // Use the find() method on the Visual collection directly
+        const visuals = await Visual.find().toArray();
+        res.json(visuals);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(3001, () => {
     console.log("Server started at port 3001");
-})
-
-
-
-console.log("Backend folder is running")
+});
